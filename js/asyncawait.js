89bab8c4f05f366aa86bc11478e11aa2c149eb47
Harendra Sijapati <harendrasijapati2012@gmail.com>
2025-05-28 18:28:52 +0545

    few chnages

--- js/asyncawait.js
+++ js/asyncawait.js
@@ -6,7 +6,7 @@ async function myFunction() {
   function myFunction() {
     return Promise.resolve("Hello");
   }
-  \
+  
   myFunction().then(
     function(value) { /* code if successful */ },
     function(error) { /* code if some error */ }
